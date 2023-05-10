6b69c961d115e0a1d8b49385f8d9b35a9cb0cf4b
Faisal <md.faisal1899@yahoo.com>
2023-05-10 10:37:57 +0600

    handle simple get request (for books)

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-const port = 3000;
+// reading the dummy json data from files
+const books = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/summaries.json`)
+);
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ app: 'bookSynop', message: 'hello from the bookS server!' });
+app.get('/api/v1/books', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: books.length,
+    data: {
+      books,
+    },
+  });
 });
 
+// running application at particular port
+const port = 3000;
 app.listen(port, () => {
   console.log('app is running....');
 });
