62590c5021225b4ad56b3d86be2b023c50f7948c
Faisal <md.faisal1899@yahoo.com>
2023-05-13 07:51:27 +0600

    connect mongoDB driver with atlas

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log('connection established');
+  });
+
+// const bookSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: [true, 'A book must have name.'],
+//   },
+//   author: {
+//     type: String,
+//     required: [true, 'Book must have author.'],
+//   },
+//   rating: {
+//     type: Number,
+//     default: 4.5,
+//   },
+// });
+
+// const Book = mongoose.model('Book', bookSchema);
 
 const port = process.env.PORT;
 app.listen(port, () => {
