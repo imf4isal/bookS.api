7f6970b06b41fd147eb7b537a291389d04281b61
Faisal <md.faisal1899@yahoo.com>
2023-05-13 16:41:49 +0600

    add dummy summaries

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  name: {
+  title: {
     type: String,
     required: [true, 'A book must have name.'],
     unique: true,
@@ -10,9 +10,34 @@ const bookSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Book must have author.'],
   },
+  coverImage: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+  summaryWriter: String,
+  writingAt: Date,
+  updatedAt: Date,
+  readingTime: Number,
   rating: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: Number,
+  topQuotes: {
+    type: [String],
+    default: [],
   },
 });
 
