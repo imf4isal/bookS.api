64e5f94eff36fd89e1f2b798a0b5ed9415239d5e
Faisal <md.faisal1899@yahoo.com>
2023-05-19 11:46:12 +0600

    handle error for Invalid Id

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,20 +1,40 @@
+const AppError = require('../utils/appError');
+
+const handleInvalidIdError = (err) => {
+  const message = `Invalid Id: ${err.value}`;
+
+  return new AppError(message, 400);
+};
+
+const developmentError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const productionError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
+    developmentError(err, res);
   }
 
   if (process.env.NODE_ENV === 'production') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
+    let error = { ...err };
+
+    if (error.kind === 'ObjectId') error = handleInvalidIdError(error);
+
+    productionError(error, res);
   }
 };
