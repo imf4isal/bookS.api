ea2ff742d33fc79c99ffcc4e62df0492105aeb6a
Faisal <md.faisal1899@yahoo.com>
2023-05-19 18:48:25 +0600

    create jwt while signing up new user->direct login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,23 @@
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
 
 exports.createUser = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    //only allow selected field
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmedPass: req.body.confirmedPass,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(200).json({
     status: 'success',
+    token,
     data: {
       newUser,
     },
