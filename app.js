d49e9f4eb2ff0df1665c521c57dadf38e52ea8bc
Faisal <md.faisal1899@yahoo.com>
2023-05-27 15:01:05 +0600

    implement security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -10,9 +15,29 @@ const errorHandler = require('./controllers/errorController');
 
 //middleware
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// limit request from same IP
+
+const limiter = rateLimit({
+  max: 50,
+  windowMS: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+// app.use(hpp({ whitelist: ['duration', 'ratingsQuantity', 'ratings'] }));
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
