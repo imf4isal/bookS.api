b87c13bba62966bdead6a71860d24bd40ade5b21
Faisal <md.faisal1899@yahoo.com>
2023-05-27 09:30:46 +0600

    send the mail with passwordReset Token to the user

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const { promisify } = require('util');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
 const AppError = require('../utils/appError');
+const sendEmail = require('./../utils/sendEmail');
 
 const generateJWT = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
@@ -118,6 +119,34 @@ exports.forgotPassword = catchAsync(async (req, res) => {
   await user.save({ validateBeforeSave: false });
 
   // send reset token to the user email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Reset your BookSynop Password(valid for 10min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500
+    );
+  }
 });
 
 exports.resetPassword = catchAsync(async (req, res) => {});
