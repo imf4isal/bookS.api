26028e22a26a35e42fbaeec5096216d614ec561e
Faisal <md.faisal1899@yahoo.com>
2023-06-01 08:43:29 +0600

    implement factory fn for updating and creating

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -19,18 +19,13 @@ exports.getReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setBookUserIds = (req, res, next) => {
   if (!req.body.book) req.body.book = req.params.summaryId;
   if (!req.body.reviewAuthor) req.body.reviewAuthor = req.user.id;
 
-  const review = await Review.create(req.body);
+  next();
+};
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review,
-    },
-  });
-});
-
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
